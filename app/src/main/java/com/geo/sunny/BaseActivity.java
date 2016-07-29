e21f9bed1551a29e2447e457e422dae89523ef4f
SunnyBoolean <1596151459@qq.com>
2016-07-29 17:52:18 +0800

    Initial commit

---
+++ app/src/main/java/com/geo/sunny/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.geo.sunny;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 李伟 on 2016/7/29.
+ */
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/geo/sunny/LoginActivity.java
@@ -0,0 +1,7 @@
+package com.geo.sunny;
+
+/**
+ * Created by 李伟 on 2016/7/29.
+ */
+public class LoginActivity {
+}
